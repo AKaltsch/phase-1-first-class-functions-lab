b1c9defc7a88e7a76b025772fafe86bd70b2b023
alec <kaltschmidalec@gmail.com>
2021-07-20 14:46:23 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+    return [arr[0], arr[1]];
+}
+
+const returnLastTwoDrivers = function (arr) {
+    return arr.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function (fare) { 
+        return fare * multiplier };
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareQuintupler = createFareMultiplier(5)
+
+const fareTripler = createFareMultiplier(3)
+
+console.log(fareQuintupler)
+
+console.log(fareQuintupler(5))
+
+// function fareDoubler(fare) {
+
+//     return doubler()(fare)
+// }
+
+function selectDifferentDrivers(arr, func) {
+    return func(arr)
+}
